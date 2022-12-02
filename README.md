7c8fb6cec2f869479c4657ab8bca5abdee22c1cc
Pratik Luitel <pluitel11@gmail.com>
2022-12-02 09:13:23 +0545

    modify readme

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 ## Excercises from Steven S. Skiena's [The Algorithm Design Manual](https://www.amazon.com/Algorithm-Design-Manual-Steven-Skiena/dp/1849967202)
 
-The programming is done in Go, and the code is arranged on the basis of chapters, leetcode, hackerrank problems and programming challenges
+The programming is done in Go, and the code is arranged on the basis of chapters, leetcode, hackerrank problems, the book's programming challenges, and miscelleneous problems I have found interesting.
 
 ## Folder structure
 ```bash
 root
+|-- utils
 |-- chapter1
 |-- chapter2
 ......
@@ -22,11 +23,17 @@ root
 |    .......
 |    |--chN_problemnameN.go
 |-- programming_challenges
-     |--ch1_problemname1.go
-     |--ch1_problemname2.go
-     |--ch2_problemname1.go
-     .......
-     |--chN_problemnameN.go
+|    |--ch1_problemname1.go
+|    |--ch1_problemname2.go
+|    |--ch2_problemname1.go
+|    .......
+|    |--chN_problemnameN.go
+|-- misc
+     |--problem1_name.go
+     |--problem2_name.go
+     ...
+     |--problemN_name.go
+
 ```
 ## To run
 In the root directory:
--- leetcode/ch2_remove_k_digits.go
+++ leetcode/ch2_remove_k_digits.go
@@ -13,39 +13,49 @@ import (
 	"fmt"
 	"strconv"
 	"time"
-
-	"github.com/pratikluitel/o_riley_algorithm_design_manual_excercises/utils"
 )
 
-// TODO working for 28/43 need to apply stack for an optimal solution
-func pre_zero_removal(num string, k int) string {
-	new_length := len(num) - k
-	fmt.Printf("Removing %d digits from %s\n", k, num)
+func removeKdigits(num string, k int) string {
+	final_len := len(num) - k
+
+	if final_len <= 0 {
+		return "0"
+	}
 	if k == 0 {
 		return num
 	}
-	if new_length <= 0 {
-		return "0"
+
+	k_removed_num := string(num[0]) //initialization
+
+	for idx := 1; idx < len(num); idx++ {
+		val := num[idx]
+		fmt.Printf("Stack: %s\n", k_removed_num)
+		if len(k_removed_num) != 0 && k_removed_num[len(k_removed_num)-1] > val {
+			fmt.Printf("curr: %s, %s > %s, Popped %s\n", string(val), string(k_removed_num[len(k_removed_num)-1]), string(val), string(k_removed_num[len(k_removed_num)-1]))
+			k_removed_num = string(k_removed_num[:len(k_removed_num)-1])
+		} else if len(k_removed_num) != final_len {
+			fmt.Printf("curr: %s, %s < %s, Pushed %s\n", string(val), string(k_removed_num), string(val), string(val))
+			k_removed_num += string(val)
+		}
+		fmt.Printf("Stack: %s\n\n", k_removed_num)
 	}
-	possible_options := num[:k+1]
-	max_idx, maximum_num := utils.StringMax(possible_options)
-	fmt.Printf("Checked in %s, minimum number: %s\n", possible_options, maximum_num)
+	// if len(k_removed_num) != final_len {
+	// 	k_removed_num = num[:final_len]
+	// }
+	// else {
+	// 	to_remove := k - len(num) + len(k_removed_num)
+	// 	k_removed_num = k_removed_num[to_remove:]
+	// }
 
-	new_num := num[:max_idx] + num[max_idx+1:] // remove the element
-
-	return pre_zero_removal(new_num, k-1)
-}
-
-func removeKdigits(num string, k int) string {
-	k_removed_num := pre_zero_removal(num, k)
+	// removing leading zeros
 	converted_int, _ := strconv.Atoi(k_removed_num)
 
 	return fmt.Sprint(converted_int)
 }
 
 func Run_4() {
-	num := "112"
-	k := 1
+	num := "2463235"
+	k := 4
 	str_removed := removeKdigits(num, k)
 	start := time.Now()
 	fmt.Printf("Start number: %s, %d strings removed: %s, execution time: %s\n", num, k, str_removed, time.Since(start))

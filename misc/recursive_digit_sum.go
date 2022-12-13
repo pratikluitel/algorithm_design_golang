e7b50e669f09ca81801881ac549c5da45f027f24
Pratik Luitel <pluitel11@gmail.com>
2022-12-13 18:42:20 +0545

    added desc

--- misc/recursive_digit_sum.go
+++ misc/recursive_digit_sum.go
@@ -1,3 +1,9 @@
+/*
+We define super digit of an integer x using the following rules:
+    - If x has only digit, then its super digit is x.
+	- Otherwise, the super digit of x is equal to the super digit of the sum of the digits of x.
+*/
+
 package misc
 
 import (

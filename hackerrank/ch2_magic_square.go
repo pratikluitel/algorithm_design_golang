b33c17bbe7d5dbb0b268368fce791d385148e9ac
Pratik Luitel <pluitel11@gmail.com>
2023-01-01 09:27:31 +0545

    added magic square

--- hackerrank/ch2_magic_square.go
+++ hackerrank/ch2_magic_square.go
@@ -16,7 +16,37 @@
 
 package hackerrank
 
-// func formingMagicSquare(s [][]int32) int32 {
-// 	// Write your code here
+import "math"
 
-// }
+func formingMagicSquare(s [][]int32) int32 {
+	costs := make([]int32, 8)
+
+	options := [8][3][3]int32{ // slice of all possible magic square of 3x3, there is no way except brute force to do this, sorry
+		{{2, 7, 6}, {9, 5, 1}, {4, 3, 8}},
+		{{6, 1, 8}, {7, 5, 3}, {2, 9, 4}},
+		{{8, 3, 4}, {1, 5, 9}, {6, 7, 2}},
+		{{4, 9, 2}, {3, 5, 7}, {8, 1, 6}},
+		{{2, 9, 4}, {7, 5, 3}, {6, 1, 8}},
+		{{4, 3, 8}, {9, 5, 1}, {2, 7, 6}},
+		{{8, 1, 6}, {3, 5, 7}, {4, 9, 2}},
+		{{6, 7, 2}, {1, 5, 9}, {8, 3, 4}},
+	}
+
+	for i, row := range s {
+		for j, value := range row {
+			for k := range costs {
+				costs[k] += int32(math.Abs(float64(options[k][i][j] - value)))
+			}
+		}
+	}
+
+	var min int32 = 0
+
+	for i, cost := range costs {
+		if cost < min || i == 0 {
+			min = cost
+		}
+	}
+
+	return min
+}
